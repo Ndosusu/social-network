ac5a0ea05b3696055127bc425497cb641314a2b4
ndossant <n.dossantos@outlook.fr>
2025-06-24 09:33:38 +0200

    on work

---
+++ backend/pkg/api/handlers/post.go
@@ -0,0 +1,17 @@
+package handlers
+
+import "net/http"
+
+func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
