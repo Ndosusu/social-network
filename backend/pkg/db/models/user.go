ff352d7e3783201f4d741772c87315a9e813fe34
alebourg <abelux662@gmail.com>
2025-06-24 14:39:49 +0200

    basic user model done + time func in utils/, next is post model

--- backend/pkg/db/models/user.go
+++ backend/pkg/db/models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"social-network/pkg/utils"
 
 	"github.com/gofrs/uuid"
 	"golang.org/x/crypto/bcrypt"
@@ -21,7 +22,7 @@ type User struct {
 	Private_mode bool
 }
 
-func (db *DB) CreateUser(obj map[string]any) Response {
+func (db *DB) InsertUser(obj map[string]any) Response {
 	/*
 		expected input (as json object) :
 		{
@@ -29,11 +30,11 @@ func (db *DB) CreateUser(obj map[string]any) Response {
 			first_name : string,
 			last_name : string,
 			password : string
-			birth_date : string,
+			date_birth : string,
 			nickname : string,
 			avatar : string
 			about : string,
-			method : CreateUser
+			method : InsertUser
 		}
 	*/
 
@@ -49,8 +50,8 @@ func (db *DB) CreateUser(obj map[string]any) Response {
 		return Response{0}
 	}
 
-	stmt := "INSERT INTO users(uuid, nickname, first_name, last_name, age, gender, email, password) VALUES (?,?,?,?,?,?,?,?);"
-	result, err := db.Conn.Exec(stmt, newUUID, obj["nickname"], obj["first_name"], obj["last_name"], obj["age"], obj["gender"], obj["email"], passwordHash)
+	stmt := "INSERT INTO users(uuid, email, password, first_name, last_name, date_birth, avatar, nick_name, about, date_creation, private_mode) VALUES (?,?,?,?,?,?,?,?,?,?,?);"
+	result, err := db.Conn.Exec(stmt, newUUID, obj["email"], passwordHash, obj["first_name"], obj["last_name"], obj["date_birth"], obj["avatar"], obj["nickname"], obj["about"], utils.GetCurrentTime(), false)
 	if err != nil {
 		fmt.Println(err)
 		return Response{0}
@@ -73,15 +74,41 @@ func (db *DB) SelectUserById(obj map[string]any) Response {
 		}
 	*/
 
-	stmt := "SELECT id, uuid, nickname, age, gender, first_name, last_name, email FROM users WHERE id = ?;"
+	stmt := "SELECT id, uuid, email, first_name, last_name, date_birth, avatar, nick_name, about, date_creation, private_mode;"
 	result := db.Conn.QueryRow(stmt, obj["id"])
 
 	user := User{}
-	err := result.Scan(&user.Id, &user.Uuid, &user.Nickname, &user.First_name, &user.Last_name, &user.Email)
+	err := result.Scan(&user.Id, &user.Uuid, &user.Email, &user.First_name, &user.Last_name, &user.Birth_date, &user.Avatar, &user.Nickname, &user.About, &user.Created_date, &user.Private_mode)
 	if err != nil {
 		fmt.Println(err)
 		return Response{User{}}
 	}
 
 	return Response{user}
+}
+
+func (db *DB) Authenticate(obj map[string]any) Response {
+	/*
+		expected input (as json object) :
+		{
+			mail : string,
+			password : string,
+			method : Authenticate
+		}
+	*/
+	var id int
+	var password []byte
+	stmt := "SELECT id, password FROM users WHERE email = ?;"
+	result := db.Conn.QueryRow(stmt, obj["mail"])
+	err := result.Scan(&id, &password)
+	if err != nil {
+		return Response{User{}}
+	}
+
+	err = bcrypt.CompareHashAndPassword(password, []byte(obj["password"].(string)))
+	if err != nil {
+		return Response{User{}}
+	}
+
+	return db.SelectUserById(map[string]any{"id": id})
 }
