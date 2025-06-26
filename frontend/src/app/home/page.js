144e6ae80812b9da09eecc697d2e0165eaa3e034
damien guimiot <damienguimiot@gmail.com>
2025-06-26 14:37:28 +0200

    organized post footer

--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -14,11 +14,19 @@ function PostList() {
                     <div className="w-full postHeader bg-primaryT p-2">
                         wiz
                     </div>
-                    <div className="w-full h-fit p-2">
+                    <div className="w-full h-fit p-4">
                         text
                     </div>
-                    <img src="/comment.svg" className="h-8"></img>
-                    <img src="/like.svg" className="h-8"></img>
+                    <div className="p-3 flex w-full gap-4">
+                        <div className="w-1/10 flex items-center">
+                            <img src="/like.svg" className="h-8"></img>
+                            <p>113k</p>
+                        </div>
+                        <div className="w-1/10 flex items-center gap-1">
+                            <img src="/comment.svg" className="h-8"></img>
+                            <p>189</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
