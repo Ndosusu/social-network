433bca4bc7cc9d08377473ba353a31e546a15948
damien guimiot <damienguimiot@gmail.com>
2025-07-10 11:18:21 +0200

    search page WIP

--- frontend/src/app/actionMenu.js
+++ frontend/src/app/actionMenu.js
@@ -15,7 +15,7 @@ export default function ActionMenu() {
     }  
 
     return (
-        <div className="neon-xl h-fit w-12 absolute top-1/3">
+        <div className="neon-xl h-fit w-12 absolute top-1/3 left-0">
             <button className="neon-sm w-10 h-10" target="home" onClick={moveTo}>H</button> {/*home page*/}
             <button className="neon-sm w-10 h-10" target="profile" onClick={moveTo}>P</button> {/*profile page*/}
             <button className="neon-sm w-10 h-10" target="search" onClick={moveTo}>S</button> {/*search page*/}
---
+++ frontend/src/app/search/page.js
@@ -0,0 +1,54 @@
+"use client"
+
+import ActionMenu from "../actionMenu"
+
+export default function SearchPage() {
+    const result = [
+        {
+            nickname: "wiz",
+            status: true,
+        },
+        {
+            nickname: "ziw",
+            status: false,
+        }
+    ]
+
+    return (
+        <div className="center w-9/10 h-full text-white flex flex-col gap-10 justify-around p-7">
+            <input type="text" placeholder="Search..." className="w-full h-fit neon-xl bg-primaryT rounded-xl p-3 text-2xl" />
+            <div className="neon-xl bg-primaryT w-full h-full rounded-xl text-2xl flex flex-col gap-4 p-6">
+                {result.map((obj, i) => <CreateSearchResult user={obj} key={i} />)}
+            </div>
+            <ActionMenu />
+        </div>
+    )
+}
+
+function CreateSearchResult(data) {
+    const user = data.user
+    return (
+        <div className="w-full h-fit">
+            <div className="flex flex-row w-full gap-5">
+                <img src="discord.svg" className="bg-discord h-15 w-15 rounded-xl" />
+                <div className="flex flex-row justify-between items-center w-full">
+                    <p>{user.nickname}</p>
+                    <CheckStatus status={user.status} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function CheckStatus(data) {
+    const status = data.status
+    if (status) {
+        return (
+            <div className="bg-green-500 h-5 w-5 rounded-full mr-5"/>
+        )
+    } else {
+        return (
+            <div className="bg-gray-500 h-5 w-5 rounded-full mr-5"/>
+        )
+    }
+}
