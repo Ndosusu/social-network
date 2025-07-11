f0fa88bbafd0d68e7b8bf045bad2b2e614007918
damien guimiot <damienguimiot@gmail.com>
2025-07-11 10:27:35 +0200

    fix notif list offset

--- frontend/src/app/notifications/page.js
+++ frontend/src/app/notifications/page.js
@@ -10,14 +10,68 @@ export default function NotificationPage() {
         },
         {
             message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb"
+        },
+        {
+            message: "bbbbb2"
         }
     ]
 
     return (
         <div className="w-screen h-screen text-white flex justify-center items-center">
-            <div className="neon-xl bg-primaryT w-9/10 h-9/10 rounded-xl text-xl p-5 overflow-hidden">
-                <p className="text-2xl font-bold">Notifications :</p>
-                <div className="w-full h-full overflow-scroll flex flex-col gap-4 mt-5 p-3">
+            <div className="neon-xl bg-primaryT w-9/10 h-9/10 rounded-xl text-xl p-5 overflow-hidden flex flex-col gap-4">
+                <p className="font-bold">Notifications :</p>
+                <div className="flex flex-col w-full h-full p-3 overflow-scroll gap-4">
                     {notifs.map((obj, i) => <CreateNotif notif={obj} key={i} />)}
                 </div>
             </div>
