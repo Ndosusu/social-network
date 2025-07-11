99079f046641de9594de32b48bdfb70ed4ac90f9
damien guimiot <damienguimiot@gmail.com>
2025-07-11 11:07:07 +0200

    Menu movement and animation

--- frontend/src/app/actionMenu.js
+++ frontend/src/app/actionMenu.js
@@ -12,17 +12,38 @@ export default function ActionMenu() {
     const logOut = async () => {
         localStorage.removeItem("logToken")
         router.push("/")
-    }  
+    }
+
+    const actionClicked = async () => {
+        const div = document.getElementById("ActionOpen")
+        const menu = document.getElementById("ActionMenu")
+        const arrow = document.getElementById("arrow")
+
+        if(div.getAttribute("opened") == "true") {
+            menu.classList.remove("neon-xl")
+            menu.classList.add("-translate-x-1/1")
+            div.setAttribute("opened", false)
+            arrow.setAttribute("src", "rightArrow.svg")
+        } else {
+            menu.classList.add("neon-xl")
+            menu.classList.remove("-translate-x-1/1")
+            div.setAttribute("opened", true)
+            arrow.setAttribute("src", "leftArrow.svg")
+        }
+    }
 
     return (
-        <div className="neon-xl h-fit w-12 absolute top-1/3 left-0">
-            <button className="neon-sm w-10 h-10" target="home" onClick={moveTo}>H</button> {/*home page*/}
-            <button className="neon-sm w-10 h-10" target="profile" onClick={moveTo}>P</button> {/*profile page*/}
-            <button className="neon-sm w-10 h-10" target="search" onClick={moveTo}>S</button> {/*search page*/}
-            <button className="neon-sm w-10 h-10" target="notifications" onClick={moveTo}>N</button> {/*notif page*/}
-            <button className="neon-sm w-10 h-10" target="chats" onClick={moveTo}>C</button> {/*chat page*/}
-            <button className="neon-sm w-10 h-10">CM</button> {/*chat modal*/}
-            <button className="bg-red-500 w-10 h-10" onClick={logOut}></button>
+        <div id="ActionMenu" className="bg-primaryT h-fit w-fit absolute inset-y-1/2 -translate-y-1/2 left-0 -translate-x-1/1 flex flex-col p-4 gap-4 rounded-br-xl rounded-tr-xl duration-500">
+            <button className="neon-sm w-12 h-12 bg-primaryT rounded-xl" target="home" onClick={moveTo}>H</button>
+            <button className="neon-sm w-12 h-12 bg-primaryT rounded-xl" target="profile" onClick={moveTo}>P</button>
+            <button className="neon-sm w-12 h-12 bg-primaryT rounded-xl" target="search" onClick={moveTo}>S</button>
+            <button className="neon-sm w-12 h-12 bg-primaryT rounded-xl" target="notifications" onClick={moveTo}>N</button>
+            <button className="neon-sm w-12 h-12 bg-primaryT rounded-xl" target="chats" onClick={moveTo}>C</button>
+            <button className="neon-sm w-12 h-12 bg-primaryT rounded-xl">CM</button>
+            <button className="neon-sm bg-red-500 w-12 h-12 rounded-xl" onClick={logOut}></button>
+            <div id="ActionOpen" className="bg-secondary neon-sm w-1/3 h-1/5 absolute -right-1/3 rounded-br-xl rounded-tr-xl p-1" onClick={actionClicked}>
+                <img id="arrow" src="rightArrow.svg" className="h-full" />
+            </div>
         </div>
     )
 }
