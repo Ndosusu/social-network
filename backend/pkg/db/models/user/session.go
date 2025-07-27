808fa852e1188a63332ec7a60dfb2999a05d7a2d
damien guimiot <damienguimiot@gmail.com>
2025-07-27 10:20:55 +0200

    fix login, typo on key name

--- backend/pkg/db/models/user/session.go
+++ backend/pkg/db/models/user/session.go
@@ -21,7 +21,7 @@ func (db *UserDB) InsertSession(userId int) (*models.Response, error) {
 		fmt.Println(err)
 		return nil, err
 	}
-	return db.GetSessionByUuid(map[string]any{"uuid": newUUID})
+	return db.GetSessionByUuid(map[string]any{"session_uuid": newUUID})
 }
 
 func (db *UserDB) GetSessionByUuid(obj map[string]any) (*models.Response, error) {
