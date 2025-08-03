48153a37b0d07f8662f99344f9d2ea95f203bc85
damien guimiot <damienguimiot@gmail.com>
2025-08-03 15:32:48 +0200

    commented file

--- backend/pkg/api/handlers/comment/update.go
+++ backend/pkg/api/handlers/comment/update.go
@@ -1,14 +1,14 @@
 package handlers_comment
 
-import (
-	"net/http"
-	"social-network/pkg/utils"
-)
+// import (
+// 	"net/http"
+// 	"social-network/pkg/utils"
+// )
 
-func UpdateCommentHandler(w http.ResponseWriter, r *http.Request) {
-	if !utils.ValidateMethod(w, r, http.MethodPut) {
-		return
-	}
+// func UpdateCommentHandler(w http.ResponseWriter, r *http.Request) {
+// 	if !utils.ValidateMethod(w, r, http.MethodPut) {
+// 		return
+// 	}
 
-	utils.JSONResponse(w, http.StatusOK, "Comment updated successfully")
-}
+// 	utils.JSONResponse(w, http.StatusOK, "Comment updated successfully")
+// }
